39e7fe6d4b0a498060a821e658b0065e05063a1a
VESOC <54740236+VESOC@users.noreply.github.com>
2022-12-10 18:00:15 +0900

    update routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/widgets.dart';
-import 'package:walking_route/screens/cart/cart_screen.dart';
 import 'package:walking_route/screens/complete_profile/complete_profile_screen.dart';
 import 'package:walking_route/screens/details/details_screen.dart';
 import 'package:walking_route/screens/forgot_password/forgot_password_screen.dart';
@@ -11,6 +10,7 @@ import 'package:walking_route/screens/sign_in/sign_in_screen.dart';
 import 'package:walking_route/screens/splash/splash_screen.dart';
 import 'package:walking_route/screens/search/search_screen.dart';
 import 'package:walking_route/screens/search_results/search_results_screen.dart';
+import 'package:walking_route/screens/new_route/new_route_screen.dart';
 
 import 'screens/sign_up/sign_up_screen.dart';
 
@@ -26,8 +26,8 @@ final Map<String, WidgetBuilder> routes = {
   OtpScreen.routeName: (context) => OtpScreen(),
   HomeScreen.routeName: (context) => HomeScreen(),
   DetailsScreen.routeName: (context) => DetailsScreen(),
-  CartScreen.routeName: (context) => CartScreen(),
   ProfileScreen.routeName: (context) => ProfileScreen(),
   SearchScreen.routeName: (context) => SearchScreen(),
+  NewRouteScreen.routeName: (context) => NewRouteScreen(),
   SearchResultScreen.routeName: (context) => SearchResultScreen(),
 };
